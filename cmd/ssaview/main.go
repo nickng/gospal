e3c5e9c286c6332a1466f887729db8cdca806a1d
Nicholas Ng <nickng@nickng.io>
2017-06-30 18:18:54 +0100

    cmd/ssaview: Tool to build and display SSA IR.

---
+++ cmd/ssaview/main.go
@@ -0,0 +1,86 @@
+// Command ssaview is a SSA printer using standard static analysis options.
+//
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/nickng/gospal/ssa/build"
+)
+
+const (
+	Usage = `ssaview is a tool for printing SSA IR of Go source code.
+
+Usage:
+
+  ssaview [options] file.go [files.go...]
+
+Options:
+
+`
+)
+
+var (
+	buildlogPath string
+	defaultArgs  bool
+	outPath      string
+
+	out io.Writer
+)
+
+func init() {
+	flag.BoolVar(&defaultArgs, "default", true, "Use default SSA build arguments")
+	flag.StringVar(&buildlogPath, "log", "", "Specify build log file (use '-' for stdout)")
+	flag.StringVar(&outPath, "out", "", "Specify output file (default: stdout)")
+}
+
+func main() {
+	flag.Parse()
+	if flag.NArg() == 0 {
+		fmt.Fprintf(os.Stderr, Usage)
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	conf := build.FromFiles(flag.Args())
+	if defaultArgs {
+		conf = conf.Default()
+	}
+
+	switch buildlogPath {
+	case "":
+	case "-":
+		conf = conf.WithBuildLog(os.Stdout, log.LstdFlags)
+	default:
+		f, err := os.Create(buildlogPath)
+		if err != nil {
+			log.Fatalf("Cannot create log %s: %v", buildlogPath, err)
+		}
+		defer f.Close()
+		conf = conf.WithBuildLog(f, log.LstdFlags)
+	}
+
+	switch outPath {
+	case "":
+		out = os.Stdout
+	default:
+		f, err := os.Create(outPath)
+		if err != nil {
+			log.Fatalf("Cannot create output file %s: %v", outPath, err)
+		}
+		defer f.Close()
+		out = f
+	}
+
+	info, err := conf.Build()
+	if err != nil {
+		log.Fatal("Cannot build SSA from files:", err)
+	}
+	if _, err := info.WriteTo(out); err != nil {
+		log.Fatal("Cannot write SSA:", err)
+	}
+}
