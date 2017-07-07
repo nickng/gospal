9ac7b54cf9dff7da5fbbb136f784ed5a837405c2
Nicholas Ng <nickng@nickng.io>
2017-07-07 13:20:51 +0100

    migoinfer: Handle Call and Go in Instruction.
    VisitCall and VisitGo are used in Block to fork the control flow with new
    Function, this commit adds MiGo generation and basic parameters handling.

--- migoinfer/internal/migoinfer/instr.go
+++ migoinfer/internal/migoinfer/instr.go
@@ -3,7 +3,9 @@ package migoinfer
 import (
 	"github.com/fatih/color"
 	"github.com/nickng/gospal/callctx"
+	"github.com/nickng/gospal/fn"
 	"github.com/nickng/gospal/funcs"
+	"github.com/nickng/gospal/store"
 	"github.com/nickng/migo"
 	"golang.org/x/tools/go/ssa"
 )
@@ -14,7 +16,8 @@ type Instruction struct {
 	callctx.Context                 // Function context.
 	Env             *Environment    // Program environment.
 
-	MiGo *migo.Function // MiGo function definition of current block.
+	MiGo      *migo.Function // MiGo function definition of current block.
+	*Exported                // Local variables.
 	*Logger
 }
 
@@ -38,6 +41,15 @@ func (v *Instruction) VisitBinOp(instr *ssa.BinOp) {
 }
 
 func (v *Instruction) VisitCall(instr *ssa.Call) {
+	def := v.createDefinition(instr.Common())
+	if def == nil {
+		return
+	}
+	if _, ok := v.Env.VisitedFunc[instr.Common()]; ok {
+		return
+	}
+	v.Env.VisitedFunc[instr.Common()] = true
+	v.doCall(instr, def)
 }
 
 func (v *Instruction) VisitChangeInterface(instr *ssa.ChangeInterface) {
@@ -65,6 +77,15 @@ func (v *Instruction) VisitFieldAddr(instr *ssa.FieldAddr) {
 }
 
 func (v *Instruction) VisitGo(instr *ssa.Go) {
+	def := v.createDefinition(instr.Common())
+	if def == nil {
+		return
+	}
+	if _, ok := v.Env.VisitedFunc[instr.Common()]; ok {
+		return
+	}
+	v.Env.VisitedFunc[instr.Common()] = true
+	v.doGo(instr, def)
 }
 
 func (v *Instruction) VisitIf(instr *ssa.If) {
@@ -86,6 +107,11 @@ func (v *Instruction) VisitMakeChan(instr *ssa.MakeChan) {
 }
 
 func (v *Instruction) VisitMakeClosure(instr *ssa.MakeClosure) {
+	def := funcs.MakeClosureDefinition(instr.Fn.(*ssa.Function), instr.Bindings)
+	v.Put(instr, def)    // For calling the closure.
+	v.Put(instr.Fn, def) // For reusing the closure.
+	f := v.Get(instr)
+	v.Logger.Debugf("%s ↳ %s", v.Logger.Module(), f.(*funcs.Definition).String())
 }
 
 func (v *Instruction) VisitMakeInterface(instr *ssa.MakeInterface) {
@@ -142,4 +168,98 @@ func (v *Instruction) SetLogger(l *Logger) {
 		SugaredLogger: l.SugaredLogger,
 		module:        color.RedString("instr"),
 	}
+}
+
+func (v *Instruction) createDefinition(c *ssa.CallCommon) *funcs.Definition {
+	if !c.IsInvoke() {
+		switch fn := c.Value.(type) {
+		case *ssa.Function, *ssa.MakeClosure:
+			def, ok := v.Get(fn).(*funcs.Definition)
+			if !ok {
+				def = funcs.MakeDefinition(c.StaticCallee())
+				v.Put(fn, def)
+			}
+			v.Logger.Debugf("%s ↳ def %s", v.Logger.Module(), def.String())
+			return def
+		case *ssa.Builtin:
+			if fn.Name() == "close" {
+				if len(c.Args) != 1 {
+					v.Logger.Fatal("%s inconsistent: close should have 1 arg",
+						v.Logger.Module())
+				}
+				exported := v.FindExported(v.Context, v.Get(c.Args[0]))
+				v.MiGo.AddStmts(&migo.CloseStatement{Chan: exported.Name()})
+			}
+			v.Logger.Debugf("%s %v", v.Logger.Module(), fn)
+		}
+		return nil
+	}
+	// Invoke mode.
+	impl := c.Value // Implementation struct/object.
+	implFn, err := fn.LookupMethodImpl(v.Env.Info.Prog, c.Method, impl)
+	if err != nil {
+		v.Logger.Infof("%s Cannot find method %v for invoke call %s",
+			v.Logger.Module(), c, c.String())
+		return nil // skip
+	}
+	def, ok := v.Get(implFn).(*funcs.Definition)
+	if !ok {
+		def = funcs.MakeDefinition(implFn)
+		v.Put(implFn, def)
+	}
+	v.Logger.Debugf("%s ↳ invoke %s", v.Logger.Module(), def.String())
+	return def
+}
+
+func (v *Instruction) doCall(c *ssa.Call, def *funcs.Definition) {
+	call := funcs.MakeCall(def, c.Common(), c)
+	v.Logger.Debugf("%s Definition: %v", v.Logger.Module(), def.String())
+	v.Logger.Debugf("%s      Call: %v", v.Logger.Module(), call.String())
+	fn := NewFunction(call, v.Context, v.Env)
+	fn.SetLogger(v.Logger)
+	v.Logger.Debugf("%s Context at caller: %s", v.Logger.Module(), v.Context)
+	v.Logger.Debugf("%s Context at callee: %v", v.Logger.Module(), fn.Context)
+
+	if len(call.Function().Blocks) == 0 {
+		// Since the function does not have body,
+		// calling it will not produce migo definitions.
+		// Instead of trying to visit the function, skip over this.
+		return
+	}
+
+	fn.EnterFunc(call.Function())
+	stmt := &migo.CallStatement{Name: fn.Callee.Name()}
+
+	// Convert type Chan parameters to MiGo parameters.
+	migoParams := paramsToMigoParam(v, fn, call)
+	stmt.AddParams(migoParams...)
+	if b, ok := fn.Analyser.(*Block); ok {
+		for _, data := range b.data {
+			data.migoFunc.AddParams(migoParams...)
+		}
+	}
+	v.MiGo.AddStmts(stmt)
+}
+
+func (v *Instruction) doGo(g *ssa.Go, def *funcs.Definition) {
+	call := funcs.MakeCall(def, g.Common(), nil)
+	v.Logger.Debugf("%s Definition: %v", v.Logger.Module(), def.String())
+	v.Logger.Debugf("%s    Go/Call: %v", v.Logger.Module(), call.String())
+	fn := NewFunction(call, v.Context, v.Env)
+	fn.SetLogger(v.Logger)
+	v.Logger.Debugf("%s Context at caller: %v", v.Logger.Module(), v.Context)
+	v.Logger.Debugf("%s Context at callee: %v", v.Logger.Module(), fn.Context)
+
+	fn.EnterFunc(call.Function())
+	stmt := &migo.SpawnStatement{Name: fn.Callee.Name()}
+
+	// Convert type Chan parameters to MiGo parameters.
+	migoParams := paramsToMigoParam(v, fn, call)
+	stmt.AddParams(migoParams...)
+	if b, ok := fn.Analyser.(*Block); ok {
+		for _, data := range b.data {
+			data.migoFunc.AddParams(migoParams...)
+		}
+	}
+	v.MiGo.AddStmts(stmt)
 }
--- migoinfer/internal/migoinfer/migo.go
+++ migoinfer/internal/migoinfer/migo.go
@@ -82,3 +82,83 @@ func migoCall(fn string, idx int, exported *Exported) migo.Statement {
 	}
 	return stmt
 }
+
+// paramsToMigoParam converts call parameters into MiGo parameters if they are
+// channel types.
+func paramsToMigoParam(v *Instruction, fn *Function, call *funcs.Call) []*migo.Parameter {
+	// Converts an argument and a function parameter pair to migo Parameter.
+	convertToMigoParam := func(arg, param store.Key) *migo.Parameter {
+		callArg := arg
+		switch ch := v.Get(callArg).(type) {
+		case store.MockValue:
+			if _, isPhi := callArg.(*ssa.Phi); isPhi {
+				v.Logger.Warnf("%s Undefined argument %s is Phi == %v",
+					v.Logger.Module(), callArg,
+					&migo.Parameter{Caller: callArg, Callee: param})
+			} else {
+				v.Logger.Warnf("%s Argument %v undefined → nil chan.\n\t%s",
+					v.Logger.Module(), callArg, v.Env.getPos(callArg))
+			}
+		case *chans.Chan:
+			if exported := v.FindExported(v.Context, ch); exported != nil {
+				callArg = exported
+			}
+		}
+		return &migo.Parameter{Caller: callArg, Callee: param}
+	}
+
+	var migoParams []*migo.Parameter
+	for i, arg := range call.Parameters {
+		arg := underlying(arg)
+		param := underlying(call.Definition().Param(i))
+		if isStruct(arg) && isStruct(param) {
+			argStruct := v.Get(arg)
+			paramStruct := fn.Get(param)
+			if mock, ok := argStruct.(store.MockValue); ok {
+				v.Logger.Warnf("%s %s is a nil struct (arg) (type:%s)",
+					v.Logger.Module(), arg.Name(), arg.Type().String())
+				argStruct = structs.New(mock, arg.(ssa.Value))
+			} else if _, ok := argStruct.(*structs.Struct); !ok {
+				argStruct = structs.New(v.Callee, arg.(ssa.Value))
+			}
+			if mock, ok := paramStruct.(store.MockValue); ok {
+				v.Logger.Warnf("%s %s is a nil struct (param) (type:%s)",
+					v.Logger.Module(), param.Name(), param.Type().String())
+				paramStruct = structs.New(mock, param.(ssa.Value))
+			} else if _, ok := paramStruct.(*structs.Struct); !ok {
+				paramStruct = structs.New(v.Callee, arg.(ssa.Value))
+			}
+			argFields := argStruct.(*structs.Struct).Expand()
+			paramFields := paramStruct.(*structs.Struct).Expand()
+			for i := 0; i < len(argFields); i++ {
+				switch argField := argFields[i].(type) {
+				case structs.SField:
+					if isChan(argField) {
+						migoParams = append(migoParams, convertToMigoParam(argField, paramFields[i]))
+					}
+				case *structs.Struct:
+					// Ignore.
+				}
+			}
+		} else if isStruct(arg) && !isStruct(param) && types.IsInterface(param.Type()) {
+			// Skips struct arg/param pair-up.
+			v.Logger.Debugf("%s Function argument is struct (type:%s), parameter is not (type:%s), likely a wildcard interface{}",
+				v.Logger.Module(), arg.Type().String(), param.Type().String())
+		}
+		if isChan(arg) {
+			migoParams = append(migoParams, convertToMigoParam(arg, call.Definition().Param(i)))
+		}
+	}
+	return migoParams
+}
+
+// underlying returns the underlying value after type assertion/interface.
+func underlying(v store.Key) store.Key {
+	switch v := v.(type) {
+	case *ssa.MakeInterface:
+		return underlying(v.X)
+	case *ssa.TypeAssert:
+		return underlying(v.X)
+	}
+	return v
+}
