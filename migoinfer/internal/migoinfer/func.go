4363986d98d88e9c1ad34cdc5ec7b1a59f343d9a
Nicholas Ng <nickng@nickng.io>
2017-07-07 13:26:03 +0100

    migoinfer: Implement Function & Package analyser.
    Function: enter, exit function.
    Package: visit init, handle global (package) vars.

--- migoinfer/internal/migoinfer/func.go
+++ migoinfer/internal/migoinfer/func.go
@@ -5,6 +5,7 @@ import (
 	"github.com/nickng/gospal/block"
 	"github.com/nickng/gospal/callctx"
 	"github.com/nickng/gospal/funcs"
+	"github.com/pkg/errors"
 	"golang.org/x/tools/go/ssa"
 )
 
@@ -41,10 +42,33 @@ func NewFunction(call *funcs.Call, ctx callctx.Context, env *Environment) *Funct
 	return &f
 }
 
+// EnterFunc enters a function and perform a context switch.
+// This should be the entry point of a function call.
 func (f *Function) EnterFunc(fn *ssa.Function) {
+	if fn == nil {
+		f.Env.Errors <- errors.Wrap(ErrFnIsNil, "When entering function")
+	}
+	defer f.ExitFunc(fn)
+	nBlock := len(f.Callee.Function().Blocks)
+	f.Logger.Debugf("%s Enter %s (%d blocks)", f.Logger.Module(), fn.Name(), nBlock)
+
+	if nBlock > 0 {
+		// This will visit all blocks in the function.
+		f.EnterBlk(f.Callee.Function().Blocks[0])
+	}
 }
 
+// ExitFunc finalises analysis of a function.
 func (f *Function) ExitFunc(fn *ssa.Function) {
+	if fn != nil {
+		f.Logger.Debugf("%s Exit %s", f.Logger.Module(), fn.Name())
+	}
+	if f.Analyser != nil {
+		// Since a function is complete analysed, we can print its content.
+		for _, data := range f.Analyser.(*Block).data {
+			f.Env.Prog.AddFunction(data.migoFunc)
+		}
+	}
 }
 
 // SetLogger sets logger for Function and its child block.Analyser.
--- migoinfer/internal/migoinfer/pkg.go
+++ migoinfer/internal/migoinfer/pkg.go
@@ -2,6 +2,8 @@ package migoinfer
 
 import (
 	"github.com/fatih/color"
+	"github.com/nickng/gospal/callctx"
+	"github.com/nickng/gospal/funcs"
 	"golang.org/x/tools/go/ssa"
 )
 
@@ -9,20 +11,37 @@ import (
 // None of the data are stored in the visitor (global variables are in shared
 // environment) so this can be reused for all packages.
 type Package struct {
-	env *Environment // Program environment
+	Env *Environment // Program environment
 	*Logger
 }
 
 func NewPackage(env *Environment) *Package {
-	return &Package{env: env}
+	return &Package{Env: env}
 }
 
 // InitGlobals initialises package-global varables in environment.
-func (p *Package) InitGlobals(*ssa.Package) {
+func (p *Package) InitGlobals(pkg *ssa.Package) {
+	for name, memb := range pkg.Members {
+		p.Logger.Debugf("%s Package member \"%s\".%s\t%T",
+			p.Logger.Module(), pkg.Pkg.Path(), name, memb)
+		switch value := memb.(type) {
+		case *ssa.Global:
+			p.Env.Globals.PutObj(value, value)
+			// TODO(nickng) handle special value kinds (array/slice/struct)
+		}
+	}
 }
 
 // VisitInit visits init function(s) in the package with a fresh context.
-func (p *Package) VisitInit(*ssa.Package) {
+func (p *Package) VisitInit(pkg *ssa.Package) {
+	if initFn := pkg.Func("init"); initFn != nil {
+		initDef := funcs.MakeCall(funcs.MakeDefinition(initFn), nil, nil)
+		fn := NewFunction(initDef, callctx.Toplevel(), p.Env)
+		fn.SetLogger(p.Logger)
+		fn.EnterFunc(initDef.Function())
+		return
+	}
+	p.Logger.Warnf("%s %s has no init", p.Logger.Module(), pkg.String())
 }
 
 // SetLogger sets logger for Package.
